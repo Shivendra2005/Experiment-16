4d4d00cc55f15fe305c67486069ff30612cbd601
Shivendra Dwivedi <146876378+Shivendra2005@users.noreply.github.com>
2024-09-13 11:49:49 +0530

    Update 16A.cpp

--- 16A.cpp
+++ 16A.cpp
@@ -1,29 +1,22 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2761
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+//SHIVENDRA DWIVEDI
+//23070123122
+#include<iostream>
+using namespace std;
 
-\f0\fs24 \cf0 //SHIVENDRA DWIVEDI\
-//23070123122\
-#include<iostream>\
-using namespace std;\
-\
-int main()\{\
-    float n1, n2, ans;\
-    cout<<"Enter values of number 1 & 2: ";\
-    cin>>n1>>n2;\
-    try\{\
-        if(n2==0)\{\
-            throw n2;\
-        \}\
-        else\{\
-            ans=n1/n2;\
-            cout<<"Answer = "<<ans<<endl;\
-        \}\
-    \}\
-    catch(float num)\{\
-        cout <<"\\nERROR: Division by "<<num<<endl;\
-    \}\
-\}}
+int main(){
+    float n1, n2, ans;
+    cout<<"Enter values of number 1 & 2: ";
+    cin>>n1>>n2;
+    try{
+        if(n2==0){
+            throw n2;
+        }
+        else{
+            ans=n1/n2;
+            cout<<"Answer = "<<ans<<endl;
+        }
+    }
+    catch(float num){
+        cout <<"\\nERROR: Division by "<<num<<endl;
+    }
+}
